15a3531f120c23e5ca6add51034a71bbe52ae0b7
Rob Landers <landers.robert@gmail.com>
2019-11-04 17:24:24 +0100

    optimize php file

--- calculate-largest-version.php
+++ calculate-largest-version.php
@@ -7,30 +7,21 @@ $all_versions = json_decode( file_get_contents( 'https://omahaproxy.appspot.com/
 
 foreach ( $all_versions as $oses ) {
 	if ( $oses['os'] === 'linux' ) {
-		$all_versions = $oses['versions'];
+		foreach ( $oses['versions'] as $version ) {
+			$active_builds[] = [
+				'version' => explode( '.', $version['current_version'] ),
+				'vs'      => $version['current_version'],
+				'channel' => $version['channel'],
+			];
+		}
 		break;
 	}
 }
+unset( $all_versions );
 
-foreach ( $all_versions as $version ) {
-	$active_builds[] = [
-		'version' => explode( '.', $version['current_version'] ),
-		'vs'      => $version['current_version'],
-		'channel' => $version['channel'],
-	];
-}
-
-$max_version = $active_builds[0]['version'];
 $versionsh   = "#!/bin/bash\n";
 
 foreach ( $active_builds as $build ) {
-
-	if ( $max_version[0] < $build['version'][0] ) {
-		$max_version = $build['version'];
-	} else if ( $max_version[0] === $build['version'][0] && $max_version[1] < $build['version'][1] ) {
-		$max_version = $build['version'];
-	}
-
 	$versionsh .= <<<SH
 echo "${build['channel']}" > versions/${build['vs']}
 
